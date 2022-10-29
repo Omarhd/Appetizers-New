b755a010ab67d7507c03c118475719eb4ee899e3
Omar <omeralhadu@gmail.com>
2022-10-29 10:25:18 +0200

    SETUP BASE AND APP DISPLAY NAME FOR SCHEMES.

---
+++ Appetizers/Configurations/ServerConfig.swift
@@ -0,0 +1,60 @@
+//
+//  ServerConfig.swift
+//  Appetizers
+//
+//  Created by Omar Abdulrahman on 29/10/2022.
+//
+
+import Foundation
+
+enum BuildSettingsKey: String {
+    
+    case BaseURL
+    
+    var value: String {
+        get {
+            return Bundle.main.infoDictionary![self.rawValue] as! String
+        }
+    }
+}
+
+class ServerConfig {
+    
+    static let shared = ServerConfig()
+    
+    var baseURL: String?
+    
+    func setupBaseUrls() {
+#if RELEASE
+        self.baseURL = BuildSettingsKey.BaseURL.value
+#elseif DEBUG
+        self.baseURL = BuildSettingsKey.BaseURL.value
+#elseif STAGING
+        self.baseURL = BuildSettingsKey.BaseURL.value
+#elseif QA
+        self.baseURL = BuildSettingsKey.BaseURL.value
+#else
+        self.baseURL = BuildSettingsKey.BaseURL.value
+#endif
+        
+    }
+    
+    func setupGoogleInfo() -> String {
+       
+        var filePath: String!
+        
+#if RELEASE
+        filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist", inDirectory: "Google-Info/Release")
+#elseif DEBUG
+        filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist", inDirectory: "Google-Info/Debug")
+#elseif STAGING
+        filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist", inDirectory: "Google-Info/Staging")
+#elseif QA
+        filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist", inDirectory: "Google-Info/AQ")
+#else
+        filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist", inDirectory: "Google-Info/Release")
+#endif
+  
+        return "filePath"
+    }
+}
