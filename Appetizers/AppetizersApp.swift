5e930e68d4a2e9a88141305c45a2a20c43e62178
Omar <omeralhadu@gmail.com>
2022-10-29 09:42:35 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,20 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Omar Abdulrahman on 29/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
